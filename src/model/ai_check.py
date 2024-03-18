e215379ab444fdd151439a4f1fb9e94df25eb1f4
Mikhail Zhelezin <mikhail.zhelezin@yandex.ru>
2024-03-18 20:22:11 +0300

    Model integration

--- src/model/ai_check.py
+++ src/model/ai_check.py
@@ -1,4 +1,23 @@
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import numpy as np
 
+
+MAX_LEN = 1024
+
+CHECKPOINT_PATH = "./deberta"
+
+tokenizer = AutoTokenizer.from_pretrained(CHECKPOINT_PATH)
+model = AutoModelForSequenceClassification.from_pretrained(
+    CHECKPOINT_PATH, max_position_embeddings=MAX_LEN
+).to("cuda")
 
 async def check_text(text: str) -> bool:
-    return False
+    inputs = tokenizer(
+        [text],
+        padding=True,
+        truncation=True,
+        max_length=MAX_LEN,
+        return_tensors="pt",
+    ).to("cuda")
+    logits = model(**inputs).logits.detach().cpu().numpy()
+    return bool(np.argmax(np.exp(logits) / np.sum(np.exp(logits), axis=-1, keepdims=True)))
